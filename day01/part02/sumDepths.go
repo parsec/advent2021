ac6e7ddbadb2534943ca29143d593340f3e6a49f
parsec <beckmanseth@gmail.com>
2021-12-03 11:04:53 -0800

    SUCCESS! SOLVED DAY 2!

--- day01/part02/sumDepths.go
+++ day01/part02/sumDepths.go
@@ -44,8 +44,12 @@ func strSliceToInt(inSlice []string) []int {
 func slidingIncrements(depthsInt []int) int {
 	increments := 0
 	for i, depths := range depthsInt {
+		if i == (len(depthsInt) - 3) { // break if we've reached the end of the slice
+			break
+		}
+
 		sumDepths := depths + depthsInt[i+1] + depthsInt[i+2]
-		sumDepthsNext := depthsInt[i+1] + depthsInt[i+2] + depthsInt[i+3]
+		sumDepthsNext := depthsInt[i+1] + depthsInt[i+2] + depthsInt[i+3] // there's probably a better way to do this that doesn't require this janky stuff
 
 		if sumDepths < sumDepthsNext {
 			increments++

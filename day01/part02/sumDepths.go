a98c92b956ba3e014ee05f8563a80c685024b802
parsec <beckmanseth@gmail.com>
2021-12-03 10:48:40 -0800

    basic functions done, working on main function now

--- day01/part02/sumDepths.go
+++ day01/part02/sumDepths.go
@@ -1,1 +1,53 @@
 package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+// error checking helper function for file reads
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+// take path to file, read file line by line into a slice of strings
+func getInput(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
+// convert []string to []int
+func strSliceToInt(inSlice []string) []int {
+	intSlice := []int{}
+	for _, i := range inSlice {
+		tmpInt, atoiErr := strconv.Atoi(i)
+		check(atoiErr)
+		intSlice = append(intSlice, tmpInt)
+	}
+	return intSlice
+}
+
+func slidingIncrements(depths []string) (int, error) {
+
+}
+
+func main() {
+	depths, inputErr := getInput("../input.txt")
+	check(inputErr)
+
+	depthsInt := strSliceToInt(depths)
+
+}

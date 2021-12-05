82853dec656e10667f0d6a8f54d425cc83e96252
parsec <beckmanseth@gmail.com>
2021-12-05 05:22:03 -0500

    I think I finished?

--- day03/part01/powerDiag.go
+++ day03/part01/powerDiag.go
@@ -2,16 +2,30 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 
 	helpers "parsec.sh/lib"
 )
 
 func calcGammaRate(diagData []string) int {
+	dataCompared := helpers.CommonBits("most", diagData)
+	rateStr := strings.Join(dataCompared, ",")
 
+	gammaRate, err := strconv.Atoi(rateStr)
+	helpers.Check(err)
+
+	return gammaRate
 }
 
 func calcEpsilonRate(diagData []string) int {
+	dataCompared := helpers.CommonBits("least", diagData)
+	rateStr := strings.Join(dataCompared, ",")
 
+	epsilonRate, err := strconv.Atoi(rateStr)
+	helpers.Check(err)
+
+	return epsilonRate
 }
 
 func main() {

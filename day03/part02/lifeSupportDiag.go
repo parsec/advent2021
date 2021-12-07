e659683149e2540d3198f26cf4b1cd82a43c24c2
parsec <beckmanseth@gmail.com>
2021-12-06 16:34:08 -0800

    Not sure what i've done wrong here.

---
+++ day03/part02/lifeSupportDiag.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	helpers "parsec.sh/lib"
+)
+
+func oxygenGenRating(diagData []string) int64 {
+	mostCommonBits := helpers.CommonBits("most", diagData)
+	o2RateBits := make([]string, len(mostCommonBits))
+	o2Rating := diagData
+
+	for i := range mostCommonBits {
+
+		for x := range o2Rating {
+			if x > (len(o2Rating) - 1) {
+				break
+			}
+			bits := []rune(o2Rating[x])
+			if string(bits[i]) != mostCommonBits[i] {
+				o2Rating[x] = o2Rating[len(o2Rating)-1]
+				o2Rating[len(o2Rating)-1] = ""
+				o2Rating = o2Rating[:len(o2Rating)-1]
+			}
+		}
+	}
+
+	rateStr := strings.Join(o2RateBits, "")
+	o2Rate, err := strconv.ParseInt(rateStr, 2, 0)
+	helpers.Check(err)
+
+	return o2Rate
+}
+
+func cO2ScrubberRating(diagData []string) int64 {
+	// todo
+}
+
+func calcLifeSupport(o2Rate int64, cO2Rate int64) int64 {
+	lifeSupportRate := o2Rate * cO2Rate
+
+	return lifeSupportRate
+}
+
+func main() {
+	diagReport, diagErr := helpers.GetInput("../input.txt")
+	helpers.Check(diagErr)
+
+	powerConsumption := calcLifeSupport(oxygenGenRating(diagReport), cO2ScrubberRating(diagReport))
+
+	fmt.Println("Power consumption: ", powerConsumption)
+}
